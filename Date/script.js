6df75affefccac43e6076ce6a5f7ffd26251e98c
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-04-18 21:40:42 +0530

    Add files via upload

---
+++ Date/script.js
@@ -0,0 +1,15 @@
+let currDate = new Date();  //Initializing Date
+console.log(currDate);
+
+console.log(currDate.toLocaleString());  //display date perfectly. 
+
+console.log(currDate.toString());     //display same as browser in terminal
+
+var d = new Date(2021, 3 , 4 , 5 , 6 , 7); //creating our own date
+console.log(d.toLocaleString());
+
+console.log(currDate.toLocaleString());
+console.log(currDate.getFullYear());
+console.log(currDate.getMonth());
+
+
