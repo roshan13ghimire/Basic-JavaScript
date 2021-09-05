b90166d4a0bbc2ab0945db988a0bf7a287c6b3fc
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-09-05 11:18:26 +0545

    Update script.js

--- Date/script.js
+++ Date/script.js
@@ -5,7 +5,7 @@ console.log(currDate.toLocaleString());  //display date perfectly.
 
 console.log(currDate.toString());     //display same as browser in terminal
 
-var d = new Date(2021, 3 , 4 , 5 , 6 , 7); //creating our own date
+var d = new Date(2021, 3 , 4 , 5 , 6 , 7); //creating our own dates
 console.log(d.toLocaleString());
 
 console.log(currDate.toLocaleString());
